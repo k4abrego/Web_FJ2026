5a3d8f0233606df73c12ba9bb10df3ee390ccd8e
k4abrego <karenabrego182@gmail.com>
2026-03-31 16:43:19 -0600

    react en raiz

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,13 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'public/unity-build/**', 'public/js/**']),
+  globalIgnores([
+    'dist',
+    '**/public/unity-build/**',
+    '**/public/js/**',
+    '**/unity-build/**',
+    '**/Overmath_FJ2026/**',
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
