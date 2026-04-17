706cf5e7c7b4497aadb80ae300a8c91925db71fa
k4abrego <karenabrego182@gmail.com>
2026-04-17 17:47:44 -0600

    cambio 548

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import ContactoPage from './pages/ContactoPage'
 import ConocenosPage from './pages/ConocenosPage'
 import HomePage from './pages/HomePage'
 import LoginTutorPage from './pages/LoginTutorPage'
-import VistaTutorPage from './pages/VistaTutorPage'
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Route path="/conocenos" element={<ConocenosPage />} />
       <Route path="/contacto" element={<ContactoPage />} />
       <Route path="/login-tutor" element={<LoginTutorPage />} />
-      <Route path="/vista-tutor" element={<VistaTutorPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
