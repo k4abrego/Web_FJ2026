8891dffa1787f5d431544a072eacf7460bcd9b0a
urnice1111 <emilianogarramxd@gmail.com>
2026-04-19 20:49:36 -0600

    Added Log In page with its type of account

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import ContactoPage from './pages/ContactoPage'
 import ConocenosPage from './pages/ConocenosPage'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 import LoginTutorPage from './pages/LoginTutorPage'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/conocenos" element={<ConocenosPage />} />
       <Route path="/contacto" element={<ContactoPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/login-tutor" element={<LoginTutorPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
